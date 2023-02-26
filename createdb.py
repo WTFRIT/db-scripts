b8d5e93c5c2c5366a68c2bc2995acc33a7665f2b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-26 10:56:36 -0500

    add some basic stuff

--- createdb.py
+++ createdb.py
@@ -1,8 +1,19 @@
 from wtfrit_storage_schema import Database
 import argparse
 
+#set up the parser
+parser = argparse.ArgumentParser()
+
+parser.add_argument('database_path', default="my_database.db", help="the path to the database file")
+
+parser.add_argument("--stamp-only", action="store_true", help="just stamp the current db version to the db")
+
+#parse the arguments
+args = parser.parse_args()
 
 if __name__ == "__main__":
-	db = Database()
-	# db.initialize()
-	db.stamp()
+	db = Database("sqlite:///" + args.database_path)
+	if args.stamp_only:
+		db.stamp()
+	else:
+		db.initialize()
